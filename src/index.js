ef3d1283b850cf5352fe54ef84caa08f1b1e979f
Nicolas-Rodriguez-Delgado <spastikcitizen@gmail.com>
2019-01-10 10:40:03 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,66 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { slide as Menu } from 'react-burger-menu'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+class Landing extends React.Component {
+    render (){
+        return (
+            <div className="Landing">
+            <MainToolBar />
+            <Logo />
+
+            <p>Find your perfect trip, designed by insiders who know and love their cities</p>
+
+            </div>
+        );
+    }
+}
+
+class Avatar extends React.Component {
+    render(){
+        return(
+            <div className="avatar">
+                <img src='/images/whiskers-sam.jpg' className="avatarPic" />
+            </div>
+        )
+    }
+}
+
+class MainToolBar extends React.Component {
+    showSettings (event) {
+        event.preventDefault();
+        
+      }
+  
+    render () {
+      return (
+          <div>
+        <Avatar />
+        <Menu right isOpen={ false }>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        </Menu>
+        </div>
+      );
+    }
+  }
+
+class Logo extends React.Component {
+    render(){
+        return(
+            <div className="Logo">
+                <img src='/images/MYtineraryLogo.png' alt="logo" className="logoPic"/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Landing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
